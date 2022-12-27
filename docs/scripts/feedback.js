fe90f73b9c3829e6f434d4621a652c5080e9980f
Axole Maranjana <82832027+Axolem@users.noreply.github.com>
2022-12-27 15:17:29 +0200

    survey complete

--- docs/scripts/feedback.js
+++ docs/scripts/feedback.js
@@ -2,22 +2,29 @@ const element = document.getElementById("app");
 
 //Create a button
 const btn = document.createElement("buttton");
-btn.setAttribute("class", "ba-we-love-subscribers")
-btn.setAttribute("onClick", "handleClick()")
+btn.setAttribute("class", "ba-we-love-subscribers");
+btn.setAttribute("onClick", "handleClick()");
 const btnText = document.createTextNode("Leave feedback.");
 btn.appendChild(btnText);
 
 //Create frame
 const div = document.createElement("div");
-div.setAttribute("class", "feedback close")
-div.setAttribute("class", "ba-we-love-subscribers.popup-ani")
+div.setAttribute("id", "feeedback");
+div.setAttribute("class", "true");
 
 const para = document.createElement("iframe");
-para.setAttribute("src", "https://www.surveymonkey.com/r/HHHG7J6")
-div.appendChild(para)
+para.setAttribute("src", "https://www.surveymonkey.com/r/HHHG7J6");
 
-function handleClick(){
-    alert("Click")
+div.appendChild(para);
+
+function handleClick() {
+  const show = document.getElementById("feeedback");
+  const itemToggle = show.getAttribute("class");
+
+  console.log(itemToggle);
+  itemToggle === "true"
+    ? show.setAttribute("class", "false")
+    : show.setAttribute("class", "true");
 }
 
 //Creating a div
